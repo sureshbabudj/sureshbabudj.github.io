949910f2fc54d7b263d168c12ea3414c98fd0733
Suresh <surdhana@in.ibm.com>
2022-03-06 14:10:30 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="intro">
+      <h1>hello, I am Suresh Babu</h1>
+      <h3>Full Stack Engineer | Front End Specialist</h3>
+      <h5>HTML/CSS, JavaScript, Angular, React, NodeJS and ExpressJS...</h5>
+    </div>
+  );
+}
